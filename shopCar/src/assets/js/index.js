316fbc0c9fe159551d25cd7061aa74db8f059670
linn <linn21@163.com>
2019-06-11 17:36:29 +0800

    first commit

---
+++ shopCar/src/assets/js/index.js
@@ -0,0 +1,32 @@
+list:[
+  {
+    id:1,
+    name:'apple air',
+    price:'6000',
+    count:1
+  },
+  {
+    id:2,
+    name:'apple pro',
+    price:'11000',
+    count:1
+  },
+  {
+    id:3,
+    name:'applex',
+    price:'7000',
+    count:1
+  },
+  {
+    id:4,
+    name:'apple8',
+    price:'4000',
+    count:1
+  },
+  {
+    id:5,
+    name:'apple7',
+    price:'3500',
+    count:1
+  },
+]
---
+++ shopCar/src/components/shopcar/shopcar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="shopcar"></div>
+</template>
+
+<script>
+    export default {
+        name: "shopcar"
+    }
+</script>
+
+<style scoped>
+
+</style>
